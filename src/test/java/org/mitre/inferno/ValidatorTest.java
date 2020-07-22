27fae9e9668740bbca10520b5b6c75abf2e6ce52
Jason Crowley <crowleyj@mitre.org>
2020-07-22 14:12:57 -0400

    Add tests for ids with version numbers

--- src/test/java/org/mitre/inferno/ValidatorTest.java
+++ src/test/java/org/mitre/inferno/ValidatorTest.java
@@ -1,5 +1,6 @@
 package org.mitre.inferno;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -106,29 +107,77 @@ public class ValidatorTest {
   @Test
   void loadIg() throws Exception {
     List<String> profilesToLoad = Arrays.asList(
+        "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-cancer-condition-parent",
+        "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-cancer-disease-status",
+        "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-cancer-genetic-variant",
+        "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-cancer-genomics-report",
+        "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-cancer-patient",
+        "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-cancer-related-medication-statement",
+        "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-cancer-related-radiation-procedure",
+        "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-cancer-related-surgical-procedure",
+        "http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-cancer-stage-parent"
+    );
+    assertTrue(profilesToLoad.stream().noneMatch(this::isProfileLoaded));
+
+    // Because the version isn't given, this should load the "current" version of hl7.fhir.us.mcode
+    List<String> profileUrls = validator.loadIg("hl7.fhir.us.mcode");
+    assertTrue(profileUrls.containsAll(profilesToLoad));
+    assertTrue(profilesToLoad.stream().allMatch(this::isProfileLoaded));
+  }
+
+  @Test
+  void loadIgWithVersions() throws Exception {
+    // A subset of the 45 profiles in hl7.fhir.us.qicore#3.3.0
+    List<String> oldProfilesToLoad = Arrays.asList(
         "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-medication",
         "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-medicationadministration",
         "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-medicationdispense",
-        "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-medicationnotdispensed",
-        "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-medicationnotrequested",
         "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-medicationrequest",
         "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-medicationstatement",
-        "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-mednotadministered",
-        "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-notDone",
-        "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-notDoneReason",
+        "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-military-service",
         "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-nutritionorder",
         "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-observation",
-        "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-observationnotdone",
         "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-organization",
         "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-patient",
         "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-practitioner",
         "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-practitionerrole",
         "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-procedure"
     );
-    assertTrue(profilesToLoad.stream().noneMatch(this::isProfileLoaded));
-    List<String> profileUrls = validator.loadIg("hl7.fhir.us.qicore");
-    assertTrue(profileUrls.containsAll(profilesToLoad));
-    assertTrue(profilesToLoad.stream().allMatch(this::isProfileLoaded));
+    assertTrue(oldProfilesToLoad.stream().noneMatch(this::isProfileLoaded));
+
+    List<String> profileUrls = validator.loadIg("hl7.fhir.us.qicore#3.3.0");
+    assertEquals(45, profileUrls.size());
+
+    // All old profiles to load have been loaded and are returned in the resulting list
+    assertTrue(profileUrls.containsAll(oldProfilesToLoad));
+    assertTrue(oldProfilesToLoad.stream().allMatch(this::isProfileLoaded));
+
+    // All of the profiles that are in hl7.fhir.us.qicore#4.9.0, but not hl7.fhir.us.qicore#3.3.0
+    List<String> newProfilesToLoad = Arrays.asList(
+        "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-communicationnotdone",
+        "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-devicenotrequested",
+        "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-doNotPerformReason",
+        "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-encounter-diagnosisPresentOnAdmission",
+        "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-immunizationnotdone",
+        "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-isElective",
+        "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-medicationnotdispensed",
+        "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-medicationnotrequested",
+        "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-mednotadministered",
+        "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-notDone",
+        "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-notDoneReason",
+        "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-observationnotdone",
+        "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-procedurenotdone",
+        "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-recorded",
+        "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-servicenotrequested"
+    );
+
+    // There are 15 added profiles and 2 removed profiles going from version 3.3.0 to 4.9.0
+    profileUrls = validator.loadIg("hl7.fhir.us.qicore#4.9.0");
+    assertEquals(58, profileUrls.size());
+
+    // All new profiles to load have been loaded and are returned in the resulting list
+    assertTrue(profileUrls.containsAll(newProfilesToLoad));
+    assertTrue(newProfilesToLoad.stream().allMatch(this::isProfileLoaded));
   }
 
   boolean isProfileLoaded(String profile) {
