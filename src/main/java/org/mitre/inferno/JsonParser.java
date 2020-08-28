ea0b06ae0d6644d628b135ec3e7ac4215948d2d8
Jason Crowley <crowleyj@mitre.org>
2020-08-28 16:53:16 -0400

    Add composePrimitive helper and fix exception message

--- src/main/java/org/mitre/inferno/JsonParser.java
+++ src/main/java/org/mitre/inferno/JsonParser.java
@@ -38,48 +38,52 @@ public class JsonParser extends org.hl7.fhir.r4.formats.JsonParser {
 
   public String composeString(Base item) {
     String type = item.fhirType();
-    if (item.isPrimitive()) {
-      String value = item.primitiveValue();
-      switch (type) {
-        case "boolean":
-        case "integer":
-        case "decimal":
-        case "unsignedInt":
-        case "positiveInt":
-          // convert to JSON boolean/number
-          return value;
-        case "string":
-        case "uri":
-        case "url":
-        case "canonical":
-        case "base64Binary":
-        case "instant":
-        case "date":
-        case "dateTime":
-        case "time":
-        case "code":
-        case "oid":
-        case "id":
-        case "markdown":
-        case "uuid":
-          // convert to JSON string
-          return new Gson().toJson(value);
-        default:
-          throw new IllegalArgumentException("Unexpected primitive type '" + type + "'.");
-      }
-    }
     try {
-      if (item.isResource()) {
+      if (item.isPrimitive()) {
+        return composePrimitive(item);
+      } else if (item.isResource()) {
         return composeString((Resource) item);
       } else if (item instanceof Type) {
         return composeString((Type) item, type);
       } else if (item instanceof BackboneElement) {
         return composeBackboneElement((BackboneElement) item);
       } else {
-        throw new IllegalArgumentException("Type '" + type + "' was not a Resource or Type.");
+        throw new IllegalArgumentException("Unsupported type '" + type + "'.");
       }
     } catch (IOException e) {
       throw new FHIRException("Failed to compose item of type '" + type + "'.", e);
+    }
+  }
+
+  private String composePrimitive(Base primitive) {
+    String type = primitive.fhirType();
+    String value = primitive.primitiveValue();
+    switch (type) {
+      case "boolean":
+      case "integer":
+      case "decimal":
+      case "unsignedInt":
+      case "positiveInt":
+        // convert to JSON boolean/number
+        return value;
+      case "string":
+      case "uri":
+      case "url":
+      case "canonical":
+      case "base64Binary":
+      case "instant":
+      case "date":
+      case "dateTime":
+      case "time":
+      case "code":
+      case "oid":
+      case "id":
+      case "markdown":
+      case "uuid":
+        // convert to JSON string
+        return new Gson().toJson(value);
+      default:
+        throw new IllegalArgumentException("Unexpected primitive type '" + type + "'.");
     }
   }
 
