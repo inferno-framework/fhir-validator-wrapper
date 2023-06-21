2724637fea32d7b8d7faa97bac7a77c1cc0efcb1
Dylan Hall <dehall@mitre.org>
2023-06-21 08:19:49 -0400

    FI-1994: bump hl7 validator version

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,10 +15,7 @@ repositories {
 }
 
 dependencies {
-    // https://chat.fhir.org/#narrow/stream/179166-implementers/topic/New.20validator.20JAR.20location
-    // the ig-publisher uses this one too
-    // https://github.com/HL7/fhir-ig-publisher/blob/master/pom.xml#L68
-    implementation("ca.uhn.hapi.fhir", "org.hl7.fhir.validation", "5.6.93")
+    implementation("ca.uhn.hapi.fhir", "org.hl7.fhir.validation", "6.0.16")
 
     // validator dependencies (should be able to get these automatically?)
     implementation("org.apache.commons","commons-compress", "1.19")
